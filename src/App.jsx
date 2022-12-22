2a6b466da55a7452bf7f64d75147a1c58d9c0058
Gino Burdea <burdea.gino@gmail.com>
2022-12-22 13:11:57 +0200

    feat: add index page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function App() {
     return (
-        <div className="app">
+        <div className="app mbc-4">
             <Outlet />
         </div>
     )
--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,3 +1,31 @@
+import '../../assets/utils.scss'
+
+const playSinglePlayer = async () => {}
+
+const joinRoom = async () => {}
+
+const createRoom = async () => {}
+
 export default function IndexPage() {
-    return <p>Index</p>
+    return (
+        <>
+            <h1>Tic-tac-toe</h1>
+
+            <div className="mbc-2">
+                <h3>Play single-player</h3>
+                <button onClick={playSinglePlayer}>Play now</button>
+            </div>
+
+            <div className="mbc-2">
+                <h3>Play multi-player</h3>
+                <div className="display-flex gap-1">
+                    <input type="text" placeholder="Room id" />
+                    <button onClick={joinRoom}>Join</button>
+                </div>
+                <p>
+                    Or <a onClick={createRoom}>create a room</a>
+                </p>
+            </div>
+        </>
+    )
 }
