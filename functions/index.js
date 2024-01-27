c7aaafffd9288a3f673399f6412fcecdd634dd92
Gino Burdea <burdea.gino@gmail.com>
2024-01-27 11:45:30 +0200

    fix: fix cors error

--- functions/index.js
+++ functions/index.js
@@ -6,137 +6,160 @@ if (admin.apps.length === 0) {
     admin.initializeApp()
 }
 
-export const createRoom = functions.https.onCall((data, context) => {
-    const run = async () => {
-        if (!context.auth?.uid) {
-            throw new functions.https.HttpsError('unauthenticated')
+const corsConfig = {
+    cors: ['tic-tac-toe-48116.netlify.app'],
+}
+
+export const createRoom = functions.https.onCall(
+    corsConfig,
+    (data, context) => {
+        const run = async () => {
+            if (!context.auth?.uid) {
+                throw new functions.https.HttpsError('unauthenticated')
+            }
+
+            const { roomId, gameData } = genGameData()
+
+            await admin
+                .firestore()
+                .collection('rooms')
+                .doc('' + roomId)
+                .set(gameData)
+            return { roomId }
         }
-
-        const { roomId, gameData } = genGameData()
-
-        await admin
-            .firestore()
-            .collection('rooms')
-            .doc('' + roomId)
-            .set(gameData)
-        return { roomId }
+        return run()
     }
-    return run()
-})
+)
 
-export const joinRoom = functions.https.onCall(({ roomId }, context) => {
-    const run = async () => {
-        if (!context.auth?.uid) {
-            throw new functions.https.HttpsError('unauthenticated')
+export const joinRoom = functions.https.onCall(
+    corsConfig,
+    ({ roomId }, context) => {
+        const run = async () => {
+            if (!context.auth?.uid) {
+                throw new functions.https.HttpsError('unauthenticated')
+            }
+
+            const roomSnap = await admin
+                .firestore()
+                .collection('rooms')
+                .doc('' + roomId)
+                .get()
+
+            if (!roomSnap.exists) {
+                throw new functions.https.HttpsError(
+                    'invalid-argument',
+                    'Room not found'
+                )
+            }
+
+            const roomData = roomSnap.data()
+
+            const indexOfPlayerId = roomData.players.indexOf(context.auth.uid)
+            if (indexOfPlayerId !== -1) {
+                return { playerId: indexOfPlayerId + 1 }
+            }
+
+            const updatedPlayersCount = roomData.playersCount + 1
+            await roomSnap.ref.update({
+                playersCount: updatedPlayersCount,
+                players: admin.firestore.FieldValue.arrayUnion(
+                    context.auth.uid
+                ),
+                gameStatus:
+                    updatedPlayersCount === 2
+                        ? 'playing'
+                        : 'waiting-for-opponent',
+            })
+
+            return { playerId: updatedPlayersCount }
         }
-
-        const roomSnap = await admin
-            .firestore()
-            .collection('rooms')
-            .doc('' + roomId)
-            .get()
-
-        if (!roomSnap.exists) {
-            throw new functions.https.HttpsError(
-                'invalid-argument',
-                'Room not found'
-            )
-        }
-
-        const roomData = roomSnap.data()
-
-        const indexOfPlayerId = roomData.players.indexOf(context.auth.uid)
-        if (indexOfPlayerId !== -1) {
-            return { playerId: indexOfPlayerId + 1 }
-        }
-
-        const updatedPlayersCount = roomData.playersCount + 1
-        await roomSnap.ref.update({
-            playersCount: updatedPlayersCount,
-            players: admin.firestore.FieldValue.arrayUnion(context.auth.uid),
-            gameStatus:
-                updatedPlayersCount === 2 ? 'playing' : 'waiting-for-opponent',
-        })
-
-        return { playerId: updatedPlayersCount }
+        return run()
     }
-    return run()
-})
+)
 
-export const leaveRoom = functions.https.onCall(({ roomId }, context) => {
-    const run = async () => {
-        if (!context.auth?.uid) {
-            throw new functions.https.HttpsError('unauthenticated')
+export const leaveRoom = functions.https.onCall(
+    corsConfig,
+    ({ roomId }, context) => {
+        const run = async () => {
+            if (!context.auth?.uid) {
+                throw new functions.https.HttpsError('unauthenticated')
+            }
+
+            const roomSnap = await admin
+                .firestore()
+                .collection('rooms')
+                .doc('' + roomId)
+                .get()
+
+            if (!roomSnap.exists) {
+                throw new functions.https.HttpsError(
+                    'invalid-argument',
+                    'Room not found'
+                )
+            }
+
+            const { gameData } = genGameData()
+
+            await roomSnap.ref.update({
+                ...gameData,
+                playersCount: admin.firestore.FieldValue.increment(-1),
+                players: admin.firestore.FieldValue.arrayRemove(
+                    context.auth.uid
+                ),
+            })
         }
-
-        const roomSnap = await admin
-            .firestore()
-            .collection('rooms')
-            .doc('' + roomId)
-            .get()
-
-        if (!roomSnap.exists) {
-            throw new functions.https.HttpsError(
-                'invalid-argument',
-                'Room not found'
-            )
-        }
-
-        const { gameData } = genGameData()
-
-        await roomSnap.ref.update({
-            ...gameData,
-            playersCount: admin.firestore.FieldValue.increment(-1),
-            players: admin.firestore.FieldValue.arrayRemove(context.auth.uid),
-        })
+        return run()
     }
-    return run()
-})
+)
 
-export const playAgain = functions.https.onCall(({ roomId }, context) => {
-    const run = async () => {
-        if (!context.auth?.uid) {
-            throw new functions.https.HttpsError('unauthenticated')
+export const playAgain = functions.https.onCall(
+    corsConfig,
+    ({ roomId }, context) => {
+        const run = async () => {
+            if (!context.auth?.uid) {
+                throw new functions.https.HttpsError('unauthenticated')
+            }
+
+            const roomSnap = await admin
+                .firestore()
+                .collection('rooms')
+                .doc('' + roomId)
+                .get()
+
+            if (!roomSnap.exists) {
+                throw new functions.https.HttpsError(
+                    'invalid-argument',
+                    'Room not found'
+                )
+            }
+
+            const roomData = roomSnap.data()
+            const { gameData } = genGameData()
+
+            const updates = {}
+            if (roomData.gameStatus === 'someone-won') {
+                updates.gameStatus = 'waiting-for-restart'
+
+                const playerId = roomData.players.indexOf(context.auth.uid) + 1
+                updates.playerRestarting = playerId
+            } else {
+                updates.playerTurn = gameData.playerTurn
+                updates.winner = gameData.winner
+                updates.winningCells = gameData.winningCells
+                updates.winningType = gameData.winningType
+                updates.cells = gameData.cells
+                updates.gameStatus = 'playing'
+                updates.playerRestarting = gameData.playerRestarting
+            }
+
+            await roomSnap.ref.update(updates)
         }
-
-        const roomSnap = await admin
-            .firestore()
-            .collection('rooms')
-            .doc('' + roomId)
-            .get()
-
-        if (!roomSnap.exists) {
-            throw new functions.https.HttpsError(
-                'invalid-argument',
-                'Room not found'
-            )
-        }
-
-        const roomData = roomSnap.data()
-        const { gameData } = genGameData()
-
-        const updates = {}
-        if (roomData.gameStatus === 'someone-won') {
-            updates.gameStatus = 'waiting-for-restart'
-
-            const playerId = roomData.players.indexOf(context.auth.uid) + 1
-            updates.playerRestarting = playerId
-        } else {
-            updates.playerTurn = gameData.playerTurn
-            updates.winner = gameData.winner
-            updates.winningCells = gameData.winningCells
-            updates.winningType = gameData.winningType
-            updates.cells = gameData.cells
-            updates.gameStatus = 'playing'
-            updates.playerRestarting = gameData.playerRestarting
-        }
-
-        await roomSnap.ref.update(updates)
+        return run()
     }
-    return run()
-})
+)
 
 export const makeAMove = functions.https.onCall(
+    corsConfig,
     ({ roomId, cellIndex }, context) => {
         const run = async () => {
             if (!context.auth?.uid) {
